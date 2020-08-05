72a20fe3dda8d4544a245f117be1a683b06d4561
Juany <nacho_cunha@hotmail.com>
2020-08-04 23:12:09 -0300

    google signIn

--- server/config/config.js
+++ server/config/config.js
@@ -25,6 +25,8 @@ if (process.env.NODE_ENV === 'dev') {
 //Expires in
 //====================
 process.env.EXPIRES_IN = 60 * 60 * 24 * 30;
+
+
 //====================
 //Autenthication seed
 //====================
@@ -32,3 +34,8 @@ process.env.EXPIRES_IN = 60 * 60 * 24 * 30;
 process.env.SEED = process.env.SEED || 'juanyss88';
 
 process.env.URLDB = urlDB;
+
+//====================
+//Google client_id 
+//====================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '642693331926-q92bkc9veqt6glo8cmb3ao28u7ko4iq0.apps.googleusercontent.com'
--- server/routes/login.js
+++ server/routes/login.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.CLIENT_ID);
+
+
 const Usuario = require('../models/usuario');
 const { response } = require('express');
 const app = express();
@@ -18,7 +22,9 @@ app.post('/login', (req, res) => {
                 ok: false,
                 err
             });
-        }
+        };
+
+
 
         if (!usuarioDB) {
             return res.status(400).json({
@@ -36,9 +42,9 @@ app.post('/login', (req, res) => {
 
         let token = jwt.sign({
             usuario: usuarioDB
-        }, process.env.SEED, { expiresIn: '1h' })
+        }, process.env.SEED, { expiresIn: process.env.EXPIRES_IN })
 
-        res.json({
+        return res.json({
             ok: true,
             usuario: usuarioDB,
             token
@@ -47,6 +53,95 @@ app.post('/login', (req, res) => {
 
     })
 });
+
+
+//Google configuration
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+
+}
+
+
+
+app.post('/google', async(req, res) => {
+    let token = req.body.idtoken;
+
+    let googleUser = await verify(token).catch(e => {
+        return res.status(403).json({
+            ok: false,
+            err: e
+        })
+    });
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        };
+
+        if (usuarioDB) {
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: 'Email ya registrado fuera de google'
+                });
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.EXPIRES_IN })
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                })
+            }
+        } else {
+            // If user don't exist on DB
+            let usuario = new Usuario();
+            usuario.nombre = googleUser.nombre,
+                usuario.email = googleUser.email,
+                usuario.img = googleUser.img,
+                usuario.google = true,
+                usuario.password = '>:D';
+
+            usuario.save((err, usuarioDB) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    })
+                };
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.EXPIRES_IN })
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                })
+            })
+
+        }
+    })
+})
 
 
 
